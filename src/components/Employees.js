d16f925adfd494f70bca4b7674df2dd43a687a5c
Navaneeth Prem <139307935+navaneethprem@users.noreply.github.com>
2023-10-27 17:48:33 +0530

    first day

---
+++ src/components/Employees.js
@@ -0,0 +1,20 @@
+const Employees = [
+    {
+        id:"1",
+        Name:"Navaneeth",
+        Age:"22"
+    },
+    {
+        id:"2",
+        Name:"Manu",
+        Age:"22"
+    },
+    {
+        id:"3",
+        Name:"Arjun",
+        Age:"21"
+    }
+
+]
+
+export default Employees;
